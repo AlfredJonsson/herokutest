5d68abaaa93cb1fa3f033cad3a21a469a1eb70bf
Alfred Jönsson <102547911+AlfredJonsson@users.noreply.github.com>
2024-10-01 17:08:16 +0200

    Add files via upload

---
+++ templates/db_handling.py
@@ -0,0 +1,17 @@
+from app import db, User, app
+
+def create_user(username, password):
+    with app.app_context():
+        user = User(username=username)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        print(f'User {username} created successfully.')
+
+if __name__ == '__main__':
+    username = input('Enter username: ')
+    password = input('Enter password: ')
+    create_user(username, password)
+
+
+
