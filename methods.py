bdb71274704f379bb5b5a9fcdee5e45b54d04fc7
Alfred Jönsson <102547911+AlfredJonsson@users.noreply.github.com>
2024-10-03 10:19:01 +0200

    method for carousel

---
+++ methods.py
@@ -0,0 +1,13 @@
+import os
+
+def get_images_as_list(directory): #get images and checks if the file name after . is accepted
+    accepted_files = {'png', 'jpg', 'jpeg', 'gif'} #we need gif? add more if needed
+    images_list = []
+
+    for file in os.listdir(directory):
+        if file.split('.')[-1].lower() in accepted_files:
+            images_list.append(file)
+    return images_list
+
+
+
